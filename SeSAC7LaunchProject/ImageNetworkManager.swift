f44e9c49d76eb91609e0ac7e152563af9e3ca24c
andev <andh1006@naver.com>
2025-09-24 11:38:39 +0900

    Swift Concurrency

---
+++ SeSAC7LaunchProject/ImageNetworkManager.swift
@@ -0,0 +1,102 @@
+//
+//  ImageNetworkManager.swift
+//  SeSAC7LaunchProject
+//
+//  Created by andev on 9/24/25.
+//
+
+import UIKit
+
+enum JackError: Error {
+    case invalidResponse
+    case unknown
+    case invalidImage
+}
+
+class ImageNetworkManager {
+    
+    static let shared = ImageNetworkManager()
+    
+    private init() { }
+    
+    static let url = URL(string: "https://picsum.photos/200/300")!
+    
+    func fetchThumbnail(completion: @escaping (UIImage) -> Void) {
+        DispatchQueue.global().async {
+            
+            if let data = try? Data(contentsOf: ImageNetworkManager.url) {
+                
+                if let image = UIImage(data: data) {
+                    
+                    DispatchQueue.main.async {
+                        completion(image)
+                    }
+                }
+            }
+        }
+    }
+    
+    func fetchThumbnailURLSession(completion: @escaping (Result<UIImage, JackError>) -> Void) {
+        
+        let request = URLRequest(url: ImageNetworkManager.url,
+                                 cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                 timeoutInterval: 5)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                guard let data else {
+                    completion(.failure(.unknown))
+                    return
+                }
+                
+                guard error == nil else {
+                    completion(.failure(.invalidResponse))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    completion(.failure(.invalidResponse))
+                    return
+                }
+                
+                guard let image = UIImage(data: data) else {
+                    completion(.failure(.invalidImage))
+                    return
+                }
+                
+                completion(.success(image))
+            }
+        }.resume()
+    }
+    
+    //Swift concurrency
+    //GCD -> 모든 걸 클로저로 해결
+    //Swfit concureencty -> 성공했으면 반환, 실패하면 오류를 던짐
+    func fetchAsyncAwait() async throws -> UIImage {
+        let request = URLRequest(url: ImageNetworkManager.url,
+                                 cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                 timeoutInterval: 5)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw JackError.invalidResponse
+        }
+        
+        guard let image = UIImage(data: data) else {
+            throw JackError.invalidImage
+        }
+        
+        return image
+    }
+    
+    func fetchAsyncLet() async throws -> [UIImage] {
+        async let one = ImageNetworkManager.shared.fetchAsyncAwait()
+        async let two = ImageNetworkManager.shared.fetchAsyncAwait()
+        async let three = ImageNetworkManager.shared.fetchAsyncAwait()
+        
+        let result = try await [one, two, three] // == GCD notify
+        
+        return result
+    }
+}
--- SeSAC7LaunchProject/ViewController.swift
+++ SeSAC7LaunchProject/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var oneImageView: UIImageView!
+    @IBOutlet var twoImageView: UIImageView!
+    @IBOutlet var threeImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //        ImageNetworkManager.shared.fetchThumbnail { image in
+        //            print("1111")
+        //            self.oneImageView.image = image
+        //        }
+        //
+        //        ImageNetworkManager.shared.fetchThumbnail { image in
+        //            print("2222")
+        //            self.twoImageView.image = image
+        //        }
+        //
+        //        ImageNetworkManager.shared.fetchThumbnail { image in
+        //            print("3333")
+        //            self.threeImageView.image = image
+        //        }
+        
+        //        //URLSession + GCD
+        //        ImageNetworkManager.shared.fetchThumbnailURLSession { response in
+        //            switch response {
+        //            case .success(let success):
+        //                self.oneImageView.image = success
+        //            case .failure(let failure):
+        //                self.oneImageView.image = UIImage(systemName: "star.fill")
+        //            }
+        //        }
+        
+        //DispatchQueue.global().async == Task
+        //        Task {
+        //            oneImageView.image = try await ImageNetworkManager.shared.fetchAsyncAwait()
+        //            print("1111")
+        //            twoImageView.image = try await ImageNetworkManager.shared.fetchAsyncAwait()
+        //            print("2222")
+        //            threeImageView.image = try await ImageNetworkManager.shared.fetchAsyncAwait()
+        //            print("3333")
+        //        }
+        
+        //GCD DispatchGroup처럼 여러 비동기 메서드를 동시에 실행
+        //Swift Concurrency async let
+        Task {
+            
+            let result = try await ImageNetworkManager.shared.fetchAsyncLet()
+            
+            oneImageView.image = result[0]
+            twoImageView.image = result[1]
+            threeImageView.image = result[2]
+        }
     }
-
-
+    
+    
 }
 
